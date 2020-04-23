9ff7d0e359c8b59c59882b5c1fe302acd5ea1cc3
Pumita <kikefontanlorenzo@gmail.com>
2020-04-23 13:25:58 +0200

    :scissors: Adding -domain shortcut

--- arguments/arguments.go
+++ arguments/arguments.go
@@ -1,43 +1,44 @@
 package arguments
 
 import (
-	"flag"
-	"fmt"
-	"os"
-	"path/filepath"
+  "flag"
+  "fmt"
+  "os"
+  "path/filepath"
 )
 
 type Arguments struct {
-	// Parser
-	argParse *flag.FlagSet
-	// Special information
-	Subcommand	string
-	Version		string
-	// Arguments
-	Domain		*string
+  // Parser
+  argParse *flag.FlagSet
+  // Special information
+  Subcommand	string
+  Version		string
+  // Arguments
+  Domain		string
 }
 
 // Constructor
 func NewProgram(version string) *Arguments {
-	arguments := &Arguments{ Version: version }
-	if len(os.Args) < 3 { arguments.ShowHelp() }
-	arguments.config()
-	return arguments
+  arguments := &Arguments{ Version: version }
+  if len(os.Args) < 3 { arguments.ShowHelp() }
+  arguments.config()
+  return arguments
 }
 
 func (arguments *Arguments) config() {
-	arguments.argParse = flag.NewFlagSet("Arguments", flag.ContinueOnError)
-	arguments.Subcommand = os.Args[1]
+  arguments.argParse = flag.NewFlagSet("Arguments", flag.ContinueOnError)
+  arguments.Subcommand = os.Args[1]
 
-	arguments.Domain = arguments.argParse.String("domain", "", "Specify the domain")
-	_ = arguments.argParse.Parse(os.Args[2:])
+  arguments.argParse.StringVar(&arguments.Domain, "d", "", "Specify the domain")
+  arguments.argParse.StringVar(&arguments.Domain, "domain", "", "Specify the domain")
+  _ = arguments.argParse.Parse(os.Args[2:])
 }
 
 func (arguments Arguments) ShowHelp() {
-	fmt.Printf("MrRobot v%s - Just another hacking framework\n\n", arguments.Version)
-	fmt.Printf("Usage: %s [subcommand] <args...>\n", filepath.Base(os.Args[0]))
-	fmt.Printf("Subcommands:\n\tsubdomain\tFind subdomains related to a given domain\n")
-	fmt.Println("Options:")
-	fmt.Println("\t-domain\t\tSpecies a domain")
-	os.Exit(1)
+  fmt.Printf("MrRobot v%s - Just another hacking framework\n\n", arguments.Version)
+  fmt.Printf("Usage: %s [subcommand] <args...>\n", filepath.Base(os.Args[0]))
+  fmt.Printf("Subcommands:\n\tsubdomain\tFind subdomains related to a given domain\n")
+  fmt.Println("Arguments:")
+  fmt.Println("\t-d, -domain\t\tSpecies a domain")
+  os.Exit(1)
 }
