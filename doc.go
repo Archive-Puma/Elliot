897e958b260b812f8a199cec7b10f8413685920d
Puma on Mint <kikefontanlorenzo@gmail.com>
2020-05-03 21:27:12 +0200

    :memo: 2nd try Documentation

--- doc.go
+++ doc.go
@@ -1,3 +1,7 @@
 /*
 Elliot is the new favorite hacking framework that will accompany you during your pentesting and intrusion tasks.
+
+For full guide visit: https://github.com/cosasdepuma/elliot
 */
+
+package elliot
