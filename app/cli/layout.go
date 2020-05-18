59f05c9ec3946944a42dfb1e7815a637ab940798
Puma on Mint <kikefontanlorenzo@gmail.com>
2020-05-18 14:48:07 +0200

    :zap: Better cyclo (<15)

--- app/cli/layout.go
+++ app/cli/layout.go
@@ -6,8 +6,97 @@ import (
 
 	"github.com/jroimartin/gocui"
 
-	plgns "github.com/cosasdepuma/elliot/app/plugins"
+	"github.com/cosasdepuma/elliot/app/plugins"
 )
+
+func displayPlugins(gui *gocui.Gui) error {
+	pluginView, err := gui.View("Plugins")
+	if err != nil {
+		return err
+	}
+	pluginView.Clear()
+	keys := make([]string, 0)
+	for key := range plugins.Plugins {
+		keys = append(keys, key)
+	}
+	sort.Strings(keys)
+	for _, key := range keys {
+		fmt.Fprintln(pluginView, key)
+	}
+	return nil
+}
+
+func displayLogger(gui *gocui.Gui) error {
+	loggerView, err := gui.View(Views[len(Views)-2].name)
+	if err != nil {
+		return err
+	}
+	loggerView.Clear()
+	switch Logger.Type {
+	case "Error":
+		loggerView.FgColor = gocui.ColorRed
+	case "Info":
+		loggerView.FgColor = gocui.ColorDefault
+	}
+	fmt.Fprintf(loggerView, "%s: %s", Logger.Type, Logger.Msg)
+	return nil
+}
+
+func displayShortcuts(gui *gocui.Gui) error {
+	shortcutsView, err := gui.View(Views[len(Views)-1].name)
+	if err != nil {
+		return err
+	}
+	shortcutsView.Clear()
+	fmt.Fprint(shortcutsView, "Shortcuts: [^C] Exit [TAB] Next Frame [Enter] Run ")
+
+	// Custom shortcuts
+	if Current == 1 {
+		fmt.Fprint(shortcutsView, "[Up|Down] Navigate")
+	}
+	return nil
+}
+
+func calculatePosition(x int, y int, w int, h int) (int, int, int, int) {
+	if x < -1 {
+		x = width + x
+	}
+	if y < -1 {
+		y = height + y
+	}
+	if w <= 0 {
+		w = width + w
+	}
+	if h <= 0 {
+		h = height + h
+	}
+	return x, y, w, h
+}
+
+func createView(gui *gocui.Gui, view *gocui.View, name string, hasFrame bool, isEditable bool, isList bool) error {
+	view.Wrap = true
+	view.Title = name
+	view.Frame = hasFrame
+	view.Editable = isList
+
+	if err := gui.SetKeybinding(name, gocui.KeyTab, gocui.ModNone, nextView); err != nil {
+		return err
+	}
+
+	if isList {
+		view.Highlight = true
+		view.SelBgColor = gocui.ColorWhite
+		view.SelFgColor = gocui.ColorBlack
+
+		if err := gui.SetKeybinding(name, gocui.KeyArrowUp, gocui.ModNone, cursorUp); err != nil {
+			return err
+		}
+		if err := gui.SetKeybinding(name, gocui.KeyArrowDown, gocui.ModNone, cursorDown); err != nil {
+			return err
+		}
+	}
+	return nil
+}
 
 func mainLayout(gui *gocui.Gui) error {
 	width, height := gui.Size()
@@ -18,95 +107,34 @@ func mainLayout(gui *gocui.Gui) error {
 
 	for index, view := range Views {
 		// Calculate position
-		x := view.x
-		if x < -1 {
-			x = width + x
-		}
-		y := view.y
-		if y < -1 {
-			y = height + y
-		}
-		w := view.w
-		if w <= 0 {
-			w = width + w
-		}
-		h := view.h
-		if h <= 0 {
-			h = height + h
-		}
+		x, y, w, h := calculatePosition(view.x, view.y, view.w, view.h)
 		// Create the view
 		if panel, err := gui.SetView(view.name, x, y, w, h); err != nil {
 			if err != gocui.ErrUnknownView {
 				return err
 			}
-			panel.Wrap = true
-			panel.Title = view.name
-			panel.Frame = view.frame
-			panel.Editable = view.editable
-
-			if err := gui.SetKeybinding(view.name, gocui.KeyTab, gocui.ModNone, nextView); err != nil {
+			if err := createView(gui, panel, view.name, view.frame, view.editable, view.list); err != nil {
 				return err
 			}
-
-			if view.list {
-				panel.Highlight = true
-				panel.SelBgColor = gocui.ColorWhite
-				panel.SelFgColor = gocui.ColorBlack
-
-				if err := gui.SetKeybinding(view.name, gocui.KeyArrowUp, gocui.ModNone, cursorUp); err != nil {
-					return err
-				}
-				if err := gui.SetKeybinding(view.name, gocui.KeyArrowDown, gocui.ModNone, cursorDown); err != nil {
-					return err
-				}
-			}
 		}
-
+		// Set the current view on top
 		if index == Current {
 			if _, err := setCurrentViewOnTop(gui, view.name); err != nil {
 				return err
 			}
 		}
 	}
-	// Display plugins
-	plugins, err := gui.View("Plugins")
-	if err != nil {
+	// Display the plugins
+	if err := displayPlugins(gui); err != nil {
 		return err
 	}
-	plugins.Clear()
-	keys := make([]string, 0)
-	for key := range plgns.Plugins {
-		keys = append(keys, key)
-	}
-	sort.Strings(keys)
-	for _, key := range keys {
-		fmt.Fprintln(plugins, key)
-	}
-	// Display logs
-	logger, err := gui.View(Views[len(Views)-2].name)
-	if err != nil {
+	// Display the logger
+	if err := displayLogger(gui); err != nil {
 		return err
 	}
-	logger.Clear()
-	switch Logger.Type {
-	case "Error":
-		logger.FgColor = gocui.ColorRed
-	case "Info":
-		logger.FgColor = gocui.ColorDefault
-	}
-	fmt.Fprintf(logger, "%s: %s", Logger.Type, Logger.Msg)
-
-	// Display shortcuts
-	shortcuts, err := gui.View(Views[len(Views)-1].name)
-	if err != nil {
+	// Display the shortcuts
+	if err := displayShortcuts(gui); err != nil {
 		return err
-	}
-	shortcuts.Clear()
-	fmt.Fprint(shortcuts, "Shortcuts: [^C] Exit [TAB] Next Frame [Enter] Run ")
-
-	// Custom shortcuts
-	if Current == 1 {
-		fmt.Fprint(shortcuts, "[Up|Down] Navigate")
 	}
 
 	return nil
