c51d1a9281282a3ad2543d18098658277f911f88
Puma on Mint <kikefontanlorenzo@gmail.com>
2020-05-23 04:15:42 +0200

    :package: Trying db

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.14
 
 require (
 	github.com/awesome-gocui/gocui v0.6.0
+	github.com/mattn/go-sqlite3 v2.0.3+incompatible // indirect
 	github.com/sirupsen/logrus v1.6.0
 )
--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
-import elliot "github.com/cosasdepuma/elliot/app"
+// import elliot "github.com/cosasdepuma/elliot/app"
 
-func main() { elliot.Entrypoint() }
+import (
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./elliot.db")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	if _, err := db.Exec("CREATE TABLE IF NOT EXISTS commoncrawl(url VARCHAR2 PRIMARY KEY)"); err != nil {
+		panic(err)
+	}
+
+	stmt, err := db.Prepare("INSERT INTO commoncrawl(url) values(?)")
+	if err != nil {
+		panic(err)
+	}
+	defer stmt.Close()
+
+	stmt.Exec("https://github.com/")
+	stmt.Exec("https://github.com/cosasdepuma/")
+
+	//elliot.Entrypoint()
+}
